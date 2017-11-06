cbb99653b94704c162c4f9e3c329e1c3aec0cd80
icnpdesign <inp@bu.edu>
2017-11-06 17:50:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code in this file!
 const currentUser = 'Izzy Nguyen' ;
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
-let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ${currentUser}!';
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}!`;
