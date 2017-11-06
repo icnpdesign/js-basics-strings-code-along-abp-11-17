0a694c60c61f32413eca5c745fd012ed034a5850
icnpdesign <inp@bu.edu>
2017-11-06 17:51:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Write your code in this file!
 const currentUser = 'Izzy Nguyen' ;
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
-let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}!`;
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
